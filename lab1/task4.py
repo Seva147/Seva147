2402e2c85ddc2a3859b0aa3d038691d6215eaf67
Seva147 <kopytovseva01@gmail.com>
2024-10-19 16:00:53 +0500

    Add files via upload

---
+++ lab1/task4.py
@@ -0,0 +1,10 @@
+import math
+
+x=float(input("Введите значение x: "))
+a=float(input("Введите значение a: "))
+y=float(input("Введите значение y: "))
+b=float(input("Введите значение b: "))
+
+z=2*x**(6/4)**(x+a)+y**b
+
+print(f"полученое значение функции z={z}")
